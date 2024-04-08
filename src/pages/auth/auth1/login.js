e1c0ecc2752e3bd1dd19ab0b37745db696f74f6b
Keval Khatri <kevalkhatri32@gmail.com>
2024-04-08 10:26:46 +0530

    setup initial route as login page

--- src/pages/auth/auth1/login.js
+++ src/pages/auth/auth1/login.js
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom';
 import { Grid, Stack, Typography } from '@mui/material';
 
 // project-imports
-import Logo from 'components/logo';
+// import Logo from 'components/logo';
 import useAuth from 'hooks/useAuth';
-import AuthSocButton from 'sections/auth/AuthSocButton';
-import AuthDivider from 'sections/auth/AuthDivider';
+// import AuthSocButton from 'sections/auth/AuthSocButton';
+// import AuthDivider from 'sections/auth/AuthDivider';
 import AuthWrapper from 'sections/auth/AuthWrapper';
 import AuthLogin from 'sections/auth/auth-forms/AuthLogin';
 
 // assets
-import imgFacebook from 'assets/images/auth/facebook.svg';
-import imgTwitter from 'assets/images/auth/twitter.svg';
-import imgGoogle from 'assets/images/auth/google.svg';
+// import imgFacebook from 'assets/images/auth/facebook.svg';
+// import imgTwitter from 'assets/images/auth/twitter.svg';
+// import imgGoogle from 'assets/images/auth/google.svg';
 
 // ================================|| LOGIN ||================================ //
 
@@ -24,10 +24,10 @@ const Login = () => {
   return (
     <AuthWrapper>
       <Grid container spacing={3}>
-        <Grid item xs={12} sx={{ textAlign: 'center' }}>
+        {/* <Grid item xs={12} sx={{ textAlign: 'center' }}>
           <Logo />
-        </Grid>
-        <Grid item xs={12}>
+        </Grid> */}
+        {/* <Grid item xs={12}>
           <Grid container spacing={1}>
             <Grid item xs={12}>
               <AuthSocButton>
@@ -45,12 +45,12 @@ const Login = () => {
               </AuthSocButton>
             </Grid>
           </Grid>
-        </Grid>
-        <Grid item xs={12}>
+        </Grid> */}
+        {/* <Grid item xs={12}>
           <AuthDivider>
             <Typography variant="body1">OR</Typography>
           </AuthDivider>
-        </Grid>
+        </Grid> */}
         <Grid item xs={12}>
           <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
             <Typography variant="h3">Login</Typography>
